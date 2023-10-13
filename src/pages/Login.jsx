78cfded5f96906eee556deaed81d2902abab1a34
Ian Kamau <kian99564@gmail.com>
2023-10-13 12:20:45 +0300

    styling login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,14 +7,38 @@ export default function Login() {
     <div>
       <Navbar />
 
-      <div>
-        <form>
+      <div className="flex justify-center item-center">
+        <form className="flex flex-col">
+          <div>
+            <h1 className="flex text-[50px]">Welcome Back</h1>
+            <h3 className="flex text-[30px]">
+              Don't have an account?{" "}
+              <NavLink className="text-blue-600" to="/signup">
+                Sign Up
+              </NavLink>
+            </h3>
+          </div>
 
-            <div>
-                <h1>Welcome Back</h1>
-                <h3>Don't have an account? <NavLink className="text-blue-600" to="/signup">Sign Up</NavLink></h3>
-            </div>
+          <label>Email:</label>
+          <input
+            type="email"
+            className="border-2 outline-none border-black w-52 rounded px-4 py-2"
+            placeholder="Enter your email address"
+          />
 
+          <label>Password:</label>
+          <input
+            type="password"
+            className="border-2 outline-none border-black w-52 rounded px-4 py-2"
+            placeholder="Enter your password"
+          />
+
+          <button
+            type="submit"
+            className="bg-blue-600 text-white w-52 py-2 outline-none rounded-lg"
+          >
+            Login
+          </button>
         </form>
       </div>
     </div>
