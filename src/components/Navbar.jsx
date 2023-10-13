c4ca3510edb03f73b283777aaa183bf8ba366ce1
Ian Kamau <kian99564@gmail.com>
2023-10-13 11:29:51 +0300

    Tailwind && navbar setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div className="flex px-12 py-8 justify-between">
+      <NavLink to="/">Home</NavLink>
+
+      <div className="flex gap-5">
+        <NavLink to="/about" className="hover:text-blue-600">
+          About
+        </NavLink>
+        <NavLink to="/blog" className="hover:text-blue-600">
+          Blog
+        </NavLink>
+        <NavLink to="/contact" className="hover:text-blue-600">
+          Contact Us
+        </NavLink>
+      </div>
+
+      <div className="flex gap-3">
+        <button>Login</button>
+        <button className="bg-blue-600 text-white outline-none rounded-lg px-4 py-1">Sign Up</button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
